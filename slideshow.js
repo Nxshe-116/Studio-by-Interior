c547c03968647afcf063754bc259a8978694b890
Nashe Chagumaira <77992898+Nxshe-116@users.noreply.github.com>
2021-11-20 05:07:09 +0200

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,21 @@
+var slideIndex = [1,2];
+
+var slideId = ["mySlides", "mySlides1","mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 2) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
